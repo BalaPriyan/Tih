83ec98fe6788a94c603ba246187dbdc20d44187a
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-01-01 11:34:07 +0530

    Update login_view.py

--- app/views/login_view.py
+++ app/views/login_view.py
@@ -3,36 +3,62 @@ from aiohttp import web
 import aiohttp_jinja2
 from aiohttp_session import new_session
 from .base import BaseView
-from firebase_admin import auth
+import pyrebase
+from flask import render_template, request, redirect, session
+from app import app
+import os
 
-class LoginView(BaseView):
-    @aiohttp_jinja2.template("login.html")
-    async def login_get(self, req: web.Request) -> web.Response:
-        return dict(authenticated=False, **req.query)
+config = {
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": "",
+    "appId": ""
+}
 
-    async def login_post(self, req: web.Request) -> web.Response:
-        post_data = await req.post()
-        redirect_to = post_data.get("redirect_to") or "/"
-        location = req.app.router["login_page"].url_for()
-        if redirect_to != "/":
-            location = location.update_query({"redirect_to": redirect_to})
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
-        if "username" not in post_data or "password" not in post_data:
-            loc = location.update_query({"error": "Username or password missing"})
-            return web.HTTPFound(location=loc)
 
-        try:
-            user = auth.get_user_by_email(post_data["username"])
-            # Authenticate user using Firebase
-            authenticated = auth.verify_password(user.uid, post_data["password"])
-        except auth.UserNotFoundError:
-            authenticated = False
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+            email = request.form['name']
+            password = request.form['password']
+            try:
+                auth.sign_in_with_email_and_password(email, password)
+                #user_id = auth.get_account_info(user['idToken'])
+                #session['usr'] = user_id
+                return render_template('home.html')
+            except:
+                unsuccessful = 'Please check your credentials'
+                return render_template('login.html', umessage=unsuccessful)
+    return render_template('login.html')
 
-        if not authenticated:
-            loc = location.update_query({"error": "Wrong Username or Password"})
-            return web.HTTPFound(location=loc)
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    if (request.method == 'POST'):
+            email = request.form['name']
+            password = request.form['password']
+            auth.create_user_with_email_and_password(email, password)
+            return render_template('login.html')
+    return render_template('create_account.html')
 
-        session = await new_session(req)
-        session["logged_in"] = True
-        session["logged_in_at"] = time.time()
-        return web.HTTPFound(location=redirect_to)
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if (request.method == 'POST'):
+            email = request.form['name']
+            auth.send_password_reset_email(email)
+            return render_template('login.html')
+    return render_template('forgot_password.html')
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run()
